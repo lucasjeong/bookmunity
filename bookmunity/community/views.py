f311e7878e0c11ca53bc7dc156f3c2289e6de756
Yongjun Jeong <lucasjeong99@naver.com>
2020-11-22 19:46:48 +0900

    .

--- bookmunity/community/views.py
+++ bookmunity/community/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+class MainView(TemplateView):
+    template_name = 'main.html'
+
+class SearchView(TemplateView):
+    template_name = 'booksearch.html'
