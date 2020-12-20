d68dbb03e43030df7e925e636fe402aeab20a30b
Yongjun Jeong <lucasjeong99@naver.com>
2020-12-20 20:59:36 +0900

    필요 없는 프로젝트들 삭제:

--- bookapi/bookfind/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BookfindConfig(AppConfig):
-    name = 'bookfind'
--- bookmunity/community/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-# Create your views here.
-
-class MainView(TemplateView):
-    template_name = 'main.html'
-
-class SearchView(TemplateView):
-    template_name = 'booksearch.html'
