a01095c4be5819b01420ed3c31618953e3663c5d
Yongjun Jeong <lucasjeong99@naver.com>
2020-12-29 21:48:02 +0900

    최종

---
+++ mysite/blog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.CharField(max_length = 200)
+    title = models.CharField(max_length = 200)
+    bookname = models.CharField(max_length = 200)
+    bookauthor = models.CharField(max_length = 200)
+    text = models.TextField()
+    published_date = models.DateTimeField(default = timezone.now())
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs = {'pk':self.pk})
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name = 'comments')
+    author = models.CharField(max_length = 200)
+    text = models.TextField()
+    published_date = models.DateTimeField(default = timezone.now())
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __stf__(self):
+        return self.text
---
+++ mysite/blog/views.py
@@ -0,0 +1,101 @@
+from __future__ import unicode_literals
+from django.shortcuts import render, get_object_or_404, redirect, render_to_response
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.template import RequestContext
+import json
+import urllib.request
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from django.utils.encoding import python_2_unicode_compatible
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit = False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk = post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form':form})
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk = pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk = post_pk)
+
+class UserCreateView(CreateView):
+    template_name = 'registration/create.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('post_list')
+
+class UserCreateDone(TemplateView):
+    template_name = 'registration/create_done.html'
+
+#책 검색
+@csrf_exempt
+def search(request):
+    if request.method == 'GET':
+
+        client_id = 'gqNyJgMrf19GMiiUJKQH'
+        client_secret = 'MMQ26KysSJ'
+
+        find = request.GET.get('find')
+        encText = urllib.parse.quote("{}".format(find))
+        url = "https://openapi.naver.com/v1/search/book?query=" + encText + "&display=10&start=1"
+
+        book_api_request = urllib.request.Request(url)
+        book_api_request.add_header("X-Naver-Client-Id", client_id)
+        book_api_request.add_header("X-Naver-Client-Secret", client_secret)
+        response = urllib.request.urlopen(book_api_request)
+
+        rescode = response.getcode()
+        if(rescode == 200):
+            response_body = response.read()
+            result = json.loads(response_body.decode('utf-8'))
+            items = result.get('items')
+
+            context = {
+                'items': items
+            }
+            return render(request, 'bookfind/bookfind.html', context = context)
